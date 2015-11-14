e9e88164f5c9e56821a006f38832b0ef660d2738
Alan Sánchez Pérez Peña <alan247@gmail.com>
2015-11-14 17:49:32 -0600

    Add objects (bio, education, work, projects)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,90 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name" : "Alan Sánchez",
+	"role" : "Web Developer",
+	"welcomeMessage" : "This is a sentence, isn't it amazing?",
+	"pictureURL" : "https://avatars0.githubusercontent.com/u/15384320?v=3&s=460",
+	"contacts" : {
+		"mobile" : "+52 1 55 8888 8888",
+		"email" : "alan247@gmail.com",
+		"github" : "alan247",
+		"location" : "Mexico City"
+	},
+	"skills" : [
+		"Web development",
+		"Guitar",
+		"Others"
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "NCCA",
+			"city" : "Mexico City",
+			"degree" : "Junior High",
+			"dates" : "??"
+		},
+		{
+			"name" : "ENP6",
+			"city" : "Mexico City",
+			"degree" : "High School",
+			"dates" : "??"
+		},
+		{
+			"name" : "University of Lodz",
+			"city" : "Lodz",
+			"degree" : "BA",
+			"dates" : "??"
+		}
+	],
+	"onlineCourses" : [
+		{
+			"title" : "Design 101",
+			"school" : "Designlab",
+			"dates" : "2015",
+			"url" : "http://trydesignlab.com"
+		},
+		{
+			"title" : "FEND Nanodegree",
+			"school" : "Udacity",
+			"dates" : "2015",
+			"url" : "http://udacity.com"
+		}
+	]
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Bricks",
+			"title" : "waiter",
+			"location" : "Mexico City",
+			"dates" : "2006 - 2007",
+			"description" : "Waiting tables with a smile"
+		},
+		{
+			"employer" : "Self",
+			"title" : "Freelancer",
+			"location" : "Around the world",
+			"dates" : "2007 - today",
+			"description" : "Working on own projects and a few external ones"
+		}
+	]
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "Porfolio",
+			"dates" : "October - November 2015",
+			"description" : "Portfolio project for Udacity Nanodegree",
+			"images" : "http://placehold.it/200/200"
+		},
+		{
+			"title" : "Responsive Blog",
+			"dates" : "Late October 2015",
+			"description" : "Blog with full responsiveness for Udacity Nanodegree",
+			"images" : "http://placehold.it/200/200"
+		}
+	]
+}
