0bd2ad0362d7e40acb3265899a717dc13dd41da2
Alan Sánchez <Alan@MacBook-Air.local>
2015-11-14 21:11:20 -0600

    Add internationalize button (modified original helper.js)

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,9 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+    //var iName = inName() || function(){};
+    var currentName = $('#name').text();
+    $('#name').text(inName(currentName));
   });
 });
 
@@ -109,9 +110,9 @@ function initializeMap() {
     disableDefaultUI: true
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
